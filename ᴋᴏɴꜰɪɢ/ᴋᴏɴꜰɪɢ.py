ee6ef05ab0290d02c30408e1fe2c99701c118b53
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-10 23:54:35 -0700

    set(int(x)

--- ᴋᴏɴꜰɪɢ/ᴋᴏɴꜰɪɢ.py
+++ ᴋᴏɴꜰɪɢ/ᴋᴏɴꜰɪɢ.py
@@ -29,5 +29,6 @@ class ӄօռʟӼ(object):
     STRICT_GBAN = True
     ALLOW_EXCL = True  
     SPAMMERS = None
+    BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
 class Faigo(ӄօռʟӼ):
     LOGGER = True
