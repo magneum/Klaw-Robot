631c0f6a2c02b58fb9e8f8bd39a7a96c1ef83aa1
HypeVoidSoul <hypevoidsoul@gmail.com>
2021-09-28 18:53:42 +0100

    𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁
    𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗞𝗿𝗮𝗸𝗶𝗻𝘇 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗟𝗮𝗯 | 𝗞𝗿𝗮𝗸𝗶𝗻𝘇𝗕𝗼𝘁

--- ӄʟǟաʀօɮօȶ/__init__.py
+++ ӄʟǟաʀօɮօȶ/__init__.py
@@ -28,27 +28,35 @@ basicConfig(
     level=INFO)
 LOGS = getLogger(__name__)
 ENV = bool(os.environ.get("ENV", False))
-TOKEN = os.environ.get("TOKEN")
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-JOIN_LOGGER = os.environ.get("JOIN_LOGGER")
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-KLAW_LINGS = set(int(x) for x in os.environ.get("KLAW_LINGS" or []))
-DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS" or []))
-EVENT_LOGS = os.environ.get("EVENT_LOGS")
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
-LOAD = os.environ.get("LOAD")
-NO_LOAD = os.environ.get("NO_LOAD")
-DEL_CMDS = os.environ.get("DEL_CMDS")
-STRICT_GBAN = os.environ.get("STRICT_GBAN")
-WORKERS = os.environ.get("WORKERS")
-BAN_STICKER = os.environ.get("BAN_STICKER")
-ALLOW_EXCL = os.environ.get("ALLOW_EXCL")
-SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT")
-SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT")
-SPAMWATCH_API = os.environ.get("SPAMWATCH_API")
-INFOPIC = ӄʟǟաʀօɮօȶ_IMG
-BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS" or []))
+TOKEN = os.environ.get("TOKEN", None)
+JOIN_LOGGER = os.environ.get("JOIN_LOGGER", None)
+EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+LOAD = os.environ.get("LOAD", "").split()
+NO_LOAD = os.environ.get("NO_LOAD", "").split()
+DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+WORKERS = int(os.environ.get("WORKERS", 8))
+BAN_STICKER = os.environ.get(
+    "BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
+SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
+SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", None)
+LOGGER = True
+OWNER_ID = 1836310130
+OWNER_USERNAME = "@HypeVoidSoul"
+SUPPORT_CHAT = "HypeVoids"
+ALLOW_CHATS = True
+DEV_USERS = [1836310130]
+KLAW_LINGS = []
+WEBHOOK = False
+INFOPIC = True
+URL = None
+DEL_CMDS = True
+STRICT_GBAN = True
+ALLOW_EXCL = True
+SPAMMERS = None
+BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
 KLAW_LINGS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
 try:
